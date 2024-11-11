1ed8078a9c6c797ca46aeb798a1541e410ef8901
decagon <udemekendrick@gmail.com>
2024-11-11 11:06:05 +0100

    entity relationship completed

--- src/main/java/com/udemekendrick/model/Department.java
+++ src/main/java/com/udemekendrick/model/Department.java
@@ -6,28 +6,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Address {
+public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false)
     private Long id;
 
     @Column(nullable = false)
-    private String streetName;
+    private String name;
 
-    @Column(nullable = false)
-    private String houseNumber;
-
-    @Column(nullable = false, unique = true)
-    private String zipCode;
-
-    @OneToOne
-    @JoinColumn(name = "employee-id")
-    private Employee employee;
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employee;
 
 }
--- src/main/java/com/udemekendrick/model/Mission.java
+++ src/main/java/com/udemekendrick/model/Mission.java
@@ -6,28 +6,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Address {
+public class Mission {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false)
     private Long id;
 
     @Column(nullable = false)
-    private String streetName;
+    private String name;
 
     @Column(nullable = false)
-    private String houseNumber;
+    private int duration;
 
-    @Column(nullable = false, unique = true)
-    private String zipCode;
-
-    @OneToOne
-    @JoinColumn(name = "employee-id")
-    private Employee employee;
+    @ManyToMany(mappedBy = "missions")
+    private List<Employee> employees;
 
 }
